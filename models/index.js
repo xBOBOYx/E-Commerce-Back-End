9007919b0d54537fb94e9e8f67c0c5dd182c3357
Anthony Zamora <anthony.zamora@gmail.com>
2021-09-08 13:30:44 -0700

    index model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'categoryId',
+}); 
 
 // Categories have many Products
-
+Category.hasMany(Product,{
+  foreignKey: 'categoryId',
+});
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+});
 
 module.exports = {
   Product,
